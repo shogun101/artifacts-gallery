149b20e963debe7ac053499f2b7cdd7324fc935b
Clawd <clawd@clawdbot.com>
2026-02-17 22:15:18 +0000

    Fix eslint disable for unused var

--- components/motion-primitives/text-effect.tsx
+++ components/motion-primitives/text-effect.tsx
@@ -180,6 +180,7 @@ const createVariantsWithTransition = (
 ): Variants => {
   if (!transition) return baseVariants;
 
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { exit: _exit, ...mainTransition } = transition;
 
   return {
