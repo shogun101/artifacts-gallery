ad70d24afd6de4f546fafccb30710223c90fc945
Clawd <clawd@clawdbot.com>
2026-02-17 14:48:20 +0000

    Mobile: sidebar compact height with gap instead of justify-between

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,7 +3,7 @@ import { Footer } from './Footer'
 
 export function Sidebar() {
   return (
-    <aside className="relative xl:fixed xl:top-0 xl:left-0 w-full xl:w-[565px] min-h-[50vh] xl:h-screen flex flex-col justify-between p-page-pad bg-black z-30">
+    <aside className="relative xl:fixed xl:top-0 xl:left-0 w-full xl:w-[565px] xl:h-screen flex flex-col gap-8 xl:gap-0 xl:justify-between p-page-pad bg-black z-30">
       <Header />
       <Footer />
     </aside>
